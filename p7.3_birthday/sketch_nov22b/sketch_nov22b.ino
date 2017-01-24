4f2d409dbbed8acbb21390e22942f901277d83b9
aimanhebat <ilovebsod@gmail.com>
2017-01-25 02:22:31 +0800

    with liquiddisplay

--- p7.3_birthday/sketch_nov22b/sketch_nov22b.ino
+++ p7.3_birthday/sketch_nov22b/sketch_nov22b.ino
@@ -1,32 +1,19 @@
-/* Play Melody
+/** Play Melody
  * -----------
  * (cleft) 2005 D. Cuartielles for K3
  * Refactoring and comments 2006 clay.shirky@nyu.edu
  * See NOTES in comments at end for possible improvements
- */
-
-#include "pitches.h"
-
-// SETUP ============================================
-// Set up speaker on a PWM pin (digital 9, 10 or 11)
-int speakerOut = 9;
-// Do we want debugging on serial out? 1 for yes, 0 for no
-int DEBUG = 0;
-
-void setup() { 
-  pinMode(speakerOut, OUTPUT);
-  if (DEBUG) { 
-    //Serial.begin(9600); // Set serial out if we want debugging
-  }
-  Serial.begin(9600); // Set serial out if we want debugging
-}
-
-// MELODY and TIMING  =======================================
-//  melody[] is an array of notes, accompanied by beats[], 
-//  which sets each note's relative length (higher #, longer note) 
-
+ **/
+ 
 // Birthday Melody
 // Note source: http://www.appybirthday.org/wp-content/uploads/2014/08/happy-birthday-song-piano-notes.jpg
+ 
+#include <LiquidCrystal.h>
+#include "pitches.h"
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int speakerOut = 9;
 
 int melody[] = {
   NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_C3, NOTE_B3, 0,
@@ -42,10 +29,10 @@ int beats[]  = {
 };
 
 char* lyrics[] = {
-  "ha", "ppy ", "birth", "day ", "to ", "you", "\n",
-  "ha", "ppy ", "birth", "day ", "to ", "you", "\n",
-  "ha", "ppy ", "birth", "day ", "to ", "Nabila", "\n",
-  "ha", "ppy ", "birth", "day ", "to ", "you!", "\n",
+  "happy", "happy", "birthday", "birthday", "to you", "to you", "to you",
+  "happy", "happy", "birthday", "birthday", "to you", "to you", "to you",
+  "happy", "happy", "birthday", "birthday", "to Nabila", "to Nabila", "to Nabila",
+  "happy", "happy", "birthday", "birthday", "to you!", "to you!", "to you!",
 };
 
 int MAX_COUNT = sizeof(melody) / 2; // Melody length, for looping.
@@ -60,10 +47,8 @@ int rest_count = 100; //<-BLETCHEROUS HACK; See NOTES
 // Initialize core variables
 int tone_ = 0;
 int beat = 0;
-long duration  = 0;
+long duration = 0;
 
-// PLAY TONE  ==============================================
-// Pulse the speaker to play a tone for a particular duration
 void playTone() {
   long elapsed_time = 0;
   if (tone_ > 0) { // if this isn't a Rest beat, while the tone has 
@@ -84,35 +69,36 @@ void playTone() {
   else { // Rest beat; loop times delay
     for (int j = 0; j < rest_count; j++) { // See NOTE on rest_count
       delayMicroseconds(duration);  
-    }                                
+    }
   }                                 
 }
 
-// LET THE WILD RUMPUS BEGIN =============================
+void setup() { 
+  pinMode(speakerOut, OUTPUT);
+  
+  lcd.begin(16, 2);
+}
+
 void loop() {
-  // Set up a counter to pull from melody[] and beats[]
-  Serial.print(lyrics[0]);
+  lcd.clear();
+  delay(2000);
   for (int i=0; i<MAX_COUNT; i++) {
+    lcd.print(".............................");
     tone_ = melody[i];
     beat = beats[i];
 
     duration = beat * tempo; // Set up timing
 
-    playTone(); 
+    playTone();
     // A pause between notes...
     delayMicroseconds(pause);
-
-    if (DEBUG) { // If debugging, report loop, tone, beat, and duration
-      Serial.print(i);
-      Serial.print(":");
-      Serial.print(beat);
-      Serial.print(" ");    
-      Serial.print(tone_);
-      Serial.print(" ");
-      Serial.println(duration);
-    }
-    
-    Serial.print(lyrics[i+1]);
+    lcd.setCursor(0, 0);
+    lcd.print(lyrics[i]);
   }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Happy birthday!");
+  lcd.setCursor(0, 1);
+  lcd.print("<3");
   exit(0);  
 }
